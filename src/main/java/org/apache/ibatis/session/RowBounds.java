e32530dbb6f66883d4a15871680e93ff638587b9
白白胖胖的空格 <541398591@qq.com>
2018-07-23 23:16:27 +0800

    添加 映射包内容，映射包贯穿 整个mybatis

--- src/main/java/org/apache/ibatis/session/RowBounds.java
+++ src/main/java/org/apache/ibatis/session/RowBounds.java
@@ -1,48 +1,63 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2017 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.session;
 
 /**
  * @author Clinton Begin
+ * 行数绑定了额
+ * 主要这个对象 set后 不能修改啊
+ *
+ * 也就是我们常说的 start limit
+ * 也就是mysql总   limit 100,10
+ *
+ * 但是mysql的 分页是逻辑分页 例如 100,10   起始取出了110条数据 只给你了最后10条
+ *
  */
 public class RowBounds {
 
-  public static final int NO_ROW_OFFSET = 0;
-  public static final int NO_ROW_LIMIT = Integer.MAX_VALUE;
-  public static final RowBounds DEFAULT = new RowBounds();
+	/**
+	 * 数据库起始行
+	 */
+	public static final int NO_ROW_OFFSET = 0;
 
-  private final int offset;
-  private final int limit;
+	/**
+	 * 然后拿到的数据
+	 */
+	public static final int NO_ROW_LIMIT = Integer.MAX_VALUE;
+	public static final RowBounds DEFAULT = new RowBounds();
 
-  public RowBounds() {
-    this.offset = NO_ROW_OFFSET;
-    this.limit = NO_ROW_LIMIT;
-  }
+	private final int offset;
+	private final int limit;
 
-  public RowBounds(int offset, int limit) {
-    this.offset = offset;
-    this.limit = limit;
-  }
+	public RowBounds() {
+		this.offset = NO_ROW_OFFSET;
+		this.limit = NO_ROW_LIMIT;
+	}
 
-  public int getOffset() {
-    return offset;
-  }
+	public RowBounds(int offset, int limit) {
+		this.offset = offset;
+		this.limit = limit;
+	}
 
-  public int getLimit() {
-    return limit;
-  }
+	public int getOffset() {
+		return offset;
+	}
+
+	public int getLimit() {
+		return limit;
+	}
 
 }
